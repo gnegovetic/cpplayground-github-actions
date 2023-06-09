37ee3d294ae80bb5ba14546013042a5f388d3641
gnegovetic <gnegovetic@hotmail.com>
2023-06-09 15:41:26 -0700

    Initial checkin.

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+    auto msg = "Hello from 'main' ---";
+    cout << msg << endl;
+
+    if (argc > 1) 
+    {
+        cout << " --- With param: " << argv[1] << endl;
+    }
+
+	return 0;
+}
